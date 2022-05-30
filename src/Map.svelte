6e94f066048d77f092062a91d4fb4ffc4f7f7de2
Blake C <blakecaven@outlook.com>
2022-05-30 14:29:34 -0400

    maybe?

--- src/Map.svelte
+++ src/Map.svelte
@@ -10,9 +10,10 @@
     }
 </script>
 
+<PotreeViewer/>
+
 {map}
 <button on:click = {() => onInfoClick(0)}> Info 1 </button> <!-- to pass a function that has parameters to on:click you have to wrap it in another function-->
 <button on:click = {() => onInfoClick(1)}> Info 2 </button>
 <button on:click = {() => start = false}> return </button>
 
-<PotreeViewer/>
